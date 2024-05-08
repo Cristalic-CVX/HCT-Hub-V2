affe2921963026b16d058edb4f868a8589292826
Cristalic-CVX <cristalixxv@gmail.com>
2024-05-08 23:24:55 +0200

    Update HCT-Hub V2.lua

--- HCT-Hub V2.lua
+++ HCT-Hub V2.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Tutorial Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 
 -- Other
