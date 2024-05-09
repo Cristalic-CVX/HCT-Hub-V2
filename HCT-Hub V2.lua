82b72ac96b8610a8d828713d3a9773821f31f095
Cristalic-CVX <cristalixxv@gmail.com>
2024-05-09 20:34:18 +0200

    Update HCT-Hub V2.lua

--- HCT-Hub V2.lua
+++ HCT-Hub V2.lua
@@ -1,8 +1,8 @@
-local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
 
-local win = DiscordLib:Window("discord library")
+local win = DiscordLib:Window("HCT Hub")
 
-local serv = win:Server("Preview", "")
+local serv = win:Server("Preview","")
 
 local btns = serv:Channel("Arsenal")
 
@@ -18,115 +18,74 @@ end)
 
 btns:Seperator()
 
-btns:Button("Blade Ball", function()
+local tgls = serv:Channel("Blade Ball")
+
+tgls:Button("Blade Ball 1", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/Loader.lua"))()
 end)
 
-btns:Seperator()
+tgls:Seperator()
 
-btns:Button("Doors-1", function()
+local sldrs = serv:Channel("Doors")
+
+sldrs:Button("Doors 1", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/Doors"))()
 end)
 
-btns:Seperator()
+sldrs:Seperator()
 
-btns:Button("Counter Blox", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fliskScript/jaran.vip/main/free.lua"))()
+local drops = serv:Channel("BloxFruits")
+
+drops:Button("BloxFruits 1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yatsuraa/Yuri/main/Winterhub_V2.lua"))()
 end)
 
-btns:Seperator()
-
-btns:Button("Slap Battles", function()
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Dusty1234567890/TestMain/main/SLAPBATTLESGUI5")))()
-end)
-
-btns:Seperator()
-
-btns:Button("BloxFruits", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yatsuraa/Yuri/main/Winterhub_V2.lua"))() 
-end)
-
-btns:Seperator()
-
-btns:Button("BrookHaven", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()  
-end)
-
-btns:Seperator()
-
-btns:Button("Da hood", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineProUser/vortexdahood/main/vortex", true))()
-end)
-
-btns:Seperator()
-
-btns:Button("Doors-2", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/Loader.lua"))()
-end)
-
-btns:Seperator()
-
-btns:Button("Made By HCT !", function()
-    print("This script have been created by HCT !")
-end)
-
-btns:Seperator()
-
-btns:Button("https://discord.gg/dj42gKFVjN", function()
-    DiscordLib:Notification("Notification", "https://discord.gg/dj42gKFVjN" , "Join Now ! ")
-end)
-local tgls = serv:Channel("Toggles")
-
-tgls:Toggle("Auto-Farm",false, function(bool)
-print(bool)
-end)
-
-local sldrs = serv:Channel("Sliders")
-
-local sldr = sldrs:Slider("Slide me!", 0, 1000, 400, function(t)
-print(t)
-end)
-
-sldrs:Button("Change to 50", function()
-sldr:Change(50)
-end)
-
-local drops = serv:Channel("Dropdowns")
-
-
-local drop = drops:Dropdown("Pick me!",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(bool)
-print(bool)
-end)
-
-drops:Button("Clear", function()
-drop:Clear()
-end)
-
-drops:Button("Add option", function()
-drop:Add("Option")
-end)
+drops:Seperator()
 
 local clrs = serv:Channel("Colorpickers")
 
-clrs:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
+clrs:Button("BloxFruits 1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fliskScript/jaran.vip/main/free.lua"))()
 end)
 
-local textbs = serv:Channel("Textboxes")
+clrs:Seperator()
 
-textbs:Textbox("Gun power", "Type here!", true, function(t)
-print(t)
+local textbs = serv:Channel("Slap Battles")
+
+textbs:Button("Slap Battles 1 ", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Dusty1234567890/TestMain/main/SLAPBATTLESGUI5")))()
 end)
 
-local lbls = serv:Channel("Labels")
+textbs:Seperator()
 
-lbls:Label("This is just a label.")
+local lbls = serv:Channel("BrookHaven")
 
-local bnds = serv:Channel("Binds")
-
-bnds:Bind("Kill bind", Enum.KeyCode.RightShift, function()
-print("Killed everyone!")
+lbls:Button("BrookHaven 1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()
 end)
 
+lbls:Seperator()
 
-win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
+local bnds = serv:Channel("Lumber Tycoon 2")
+
+bnds:Button("Lumber Tycoon 2 script 1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()
+end)
+
+local Dahood = serv:Channel("Da hood")
+
+Dahood:Button("Da hood 1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineProUser/vortexdahood/main/vortex", true))()
+end)
+
+local TOW = serv:Channel("Tower of Hell")
+
+TOW:Button("Tower of Hell 1", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/sc0ttferren/robloxscripts/main/toh/sc0tt.lua'))()
+end)
+
+local RCK = serv:Channel("Race Clicker")
+
+RCK:Button("Race Clicker 1", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/CUkKFaQy"))()
+end)
